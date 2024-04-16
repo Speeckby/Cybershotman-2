718c1148c2700be3708bb1581c487671cd74a4dd
cloqtn <125363694+cloqtn@users.noreply.github.com>
2024-04-16 10:28:56 +0200

    projectile class

---
+++ src/Projectile.cpp
@@ -0,0 +1,23 @@
+#include "Projectile.h"
+#include <cmath>
+
+Projectile::Projectile(int xperso, int yperso, int xviseur, int yviseur)
+{
+    Caracteristiques* m_cara;
+    m_cara = new Caracteristique(xperso, yperso, 0, 20, 10);
+
+    float alpha = atan(yviseur-yperso/xviseur-xperso);
+    float dx = cos(alpha)*m_cara.m_vit;
+    float dy = sin(alpha)*m_cara.m_vit;
+}
+
+Projectile::updatePos()
+{
+    m_cara.m_x = m_cara.m_x + dx;
+    m_cara.m_y = m_cara.m_y + dy;
+}
+
+Projectile::~Projectile()
+{
+    //dtor
+}
