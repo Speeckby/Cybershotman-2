164466a62185940c7bd02b4bd09c3f67aacf58a0
cloqtn <125363694+cloqtn@users.noreply.github.com>
2024-05-24 11:46:42 +0200

    Update Perso.cpp

--- src/Perso.cpp
+++ src/Perso.cpp
@@ -1,23 +1,34 @@
 #include "../include/Perso.h"
 
-Perso::Perso(Caracteristiques* caracteristiques)
+Perso::Perso(Caracteristique* caracteristiques)
 {
-    donnees=caracteristiques;
+    donnees = caracteristiques;
 }
 
-Perso::est_mort()
+bool Perso::est_mort()
 {
-    return donnees.hp <= 0;
+    return donnees->m_hp <= 0;
 }
 void Perso::deplacer_perso(expansion_t exp)
 {
-    donnees->m_x -= (exp.nunchuk.js.pos.x - 127)/20;
-    donnees->m_y += (exp.nunchuk.js.pos.y - 125)/20;
+    donnees->m_x -= ((exp.nunchuk.js.pos.x - 127)/-20)*donnees->m_vit;
+    donnees->m_y += ((exp.nunchuk.js.pos.y - 125)/-20)*donnees->m_vit;
 }
 
 void Perso::afficher_perso()
 {
-    GRRLIB_DrawTile(donnees->m_x, donnees->m_y, donnees->m_image, 0,1,1,  0xFFFFFFFF, 0);
+    GRRLIB_DrawTile(donnees->m_x, donnees->m_y, donnees->m_image, 0,2,2,  0xFFFFFFFF, 0);
+}
+void Perso::init_perso()
+{
+    GRRLIB_texImg* tuiles_perso = GRRLIB_LoadTextureFromFile("sd:/survivor/player/purple_sprite.png");
+    GRRLIB_InitTileSet(tuiles_perso, 32,32,0);
+
+}
+
+void Perso::tirer(set <Projectile> projectiles, ir_t ir)
+{
+    projectiles.insert(new Projectile(donnees->GetX(),donnees->GetY(), ir.x, ir.y))
 }
 
 Perso::~Perso()
