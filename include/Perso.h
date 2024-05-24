8a821401f0fa9e104fead318be51c1dae53b4eb4
cloqtn <125363694+cloqtn@users.noreply.github.com>
2024-05-24 11:46:07 +0200

    Update Perso.h

--- include/Perso.h
+++ include/Perso.h
@@ -1,16 +1,24 @@
 #ifndef PERSO_H
 #define PERSO_H
 #include "Caracteristique.h"
+#include "Inventaire.h"
+#include <wiiuse/wiiuse.h>
+#include "Projectile.h"
 
 
 class Perso
 {
     public:
         Perso(Caracteristique* caracteristiques);
+        void deplacer_perso(expansion_t exp);
+        bool est_mort();
+        void afficher_perso();
+        void init_perso();
+        void tirer(set <Projectile> projectiles, ir_t ir);
         virtual ~Perso();
 
         Caracteristique* donnees;
-        Equipement* inventaire;
+        Inventaire* inventaire;
     protected:
 
     private:
