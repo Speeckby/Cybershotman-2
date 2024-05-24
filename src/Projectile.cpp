9aa448df45d669b4dcd04f32b3b8950bc3cfedb8
cloqtn <125363694+cloqtn@users.noreply.github.com>
2024-05-24 11:47:12 +0200

    Update Projectile.cpp

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -4,12 +4,12 @@
 
 Projectile::Projectile(int xperso, int yperso, int xviseur, int yviseur)
 {
-    GRRLIB_texImg* img = GRRLIB_LoadTextureFromFile("sd:/Cybershotman2/Projectile_joueur.png");
+    GRRLIB_texImg* img = GRRLIB_LoadTextureFromFile("sd:/survivor/projectile.png");
     m_carac = new Caracteristique(xperso, yperso, 0, 20, 10, img);
-
+    GRRLIB_InitTileSet(img,32,32,0);
     float alpha = atan(yviseur-yperso/xviseur-xperso);
     float dx = cos(alpha)*m_carac->m_vit;
-    float dy = sin(alpha)*m_carac->m_vit;
+    float dy = sin(alpha)*m_carac->m_vit;rt_
 }
 
 void Projectile::updatePos()
@@ -18,7 +18,26 @@ void Projectile::updatePos()
     m_carac->m_y += dy;
 }
 
+bool Projectile::isHidden()
+{
+    if (m_carac->GetX() > 640) return true;
+    if (m_carac->GetX() < 0) return true;
+    if (m_carac->GetY() > 480) return true;
+    if (m_carac->GetY() < 0) return true;
+
+    return false;
+}
+
+void Projectile::display()
+{
+    GRRLIB_DrawImg(m_carac->GetX(),m_carac->GetY(),img,0,1,1,0xFFFFFFFF);
+}
+
 Projectile::~Projectile()
 {
-    //dtor
+    delete[] img;
+    delete[] m_carac;
+    delete[] alpha;
+    delete[] dx;
+    delete[] dy;
 }
